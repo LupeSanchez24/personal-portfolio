062f617c3f4e95815572c366676816452ddbe987
GuadalupeSanchez <guadalupesanchezp6@gmail.com>
2025-02-01 15:30:06 -0800

    Added css to main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,10 +2,13 @@ import "./Main.css";
 function Main() {
   return (
     <main className="main">
-      <div className="main__title">
-        {" "}
-        Hi, I am Guadalupe Sanchez a Frontend Developer from Washington
-      </div>
+      <h2 className="main__title">
+        Hi, I am <span className="main__title-name">Guadalupe Sanchez</span>{" "}
+      </h2>
+      <p className="main__description">
+        a <span className="main__description-position">Frontend Developer</span>{" "}
+        from Washington
+      </p>
     </main>
   );
 }
